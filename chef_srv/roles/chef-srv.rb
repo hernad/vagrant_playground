d971a4794ec1f94dcd26364c8e24d1ee215147cf
Ernad Husremovic <hernad@bring.out.ba>
2011-08-13 19:18:48 +0200

    chef-1

--- chef_srv/roles/chef-srv.rb
+++ chef_srv/roles/chef-srv.rb
@@ -4,8 +4,10 @@ description "vagrant chef-server ntp i upgrade chef na 0.10.2"
 
 run_list [
   "recipe[ntp]",
+  "recipe[hosts]",
   "recipe[chef-server::rubygems-install]", 
   "recipe[chef-server::default]",
-  "recipe[upgrade_chef_0.10.0_0.10.2]"
+  "recipe[chef_daemon_bin_sbin::default]",
+  "recipe[chef_upgrade_0.10.0_0.10.2]"
 
 ]
